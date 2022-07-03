8300566e7a9411e97ce9b4916ef0ef077c1275b2
Victor Araujo <victor_emanuel9@hotmail.com>
2022-07-03 14:14:52 -0300

    chore: prisma ORM setup

---
+++ backend/prisma/migrations/20220703163222_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE `Task` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(100) NOT NULL,
+    `content` VARCHAR(500) NOT NULL,
+    `status` VARCHAR(20) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
