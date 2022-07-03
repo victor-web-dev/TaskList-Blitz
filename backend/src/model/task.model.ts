61a6cff3d1d1b23c761d4c3ae8582b742a4795bd
Victor Araujo <victor_emanuel9@hotmail.com>
2022-07-03 17:49:03 -0300

    feat: getAll implemented

--- backend/src/model/task.model.ts
+++ backend/src/model/task.model.ts
@@ -1,15 +1,31 @@
 import { PrismaClient } from '@prisma/client';
 import { ITaskData, ITaskModel } from '../interfaces/task.interface';
+import Log from '../logs/generate.log';
 
 export default class TaskModel implements ITaskModel {
   private prisma: PrismaClient;
 
+  private logPath: string;
+
+  private log: Log;
+
   constructor() {
     this.prisma = new PrismaClient();
+    this.logPath = '../logs/model/taskLog.txt';
+    this.log = new Log(this.logPath);
   }
 
-  async getAll(): Promise<ITaskData[]> {
-    throw new Error('Method not implemented.');
+  // eslint-disable-next-line consistent-return
+  public async getAll(): Promise<ITaskData[] | undefined> {
+    try {
+      const data = await this.prisma.task.findMany();
+      if (data.length < 1) return [];
+      return data as ITaskData[];
+    } catch (error) {
+      if (error instanceof Error) {
+        this.log.generate(error.message);
+      }
+    }
   }
 
   async createTask() {
