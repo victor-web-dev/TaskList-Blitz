17325d30472fb13760ac8283df52d86d5a5037d0
Victor Araujo <victor_emanuel9@hotmail.com>
2022-07-04 20:03:53 -0300

    build: setup react-router-dom and axios

---
+++ frontend/src/Components/Home/index.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+
+function Home() {
+  const [allTasks, setAllTasks] = useState([]);
+  return (
+    <>
+      <h2>Teste</h2>
+    </>
+  );
+}
+
+export default Home;
